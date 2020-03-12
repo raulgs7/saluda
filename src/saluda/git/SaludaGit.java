563b07385b46c162a84c6848371154d43a292fa9
Alumno <Alumno@aula006-09>
2020-03-12 04:46:00 +0100

    Primer commit

---
+++ src/saluda/git/SaludaGit.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saluda.git;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+public class SaludaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+        System.out.println("Introduzca su nombre:");
+        nombre = sc.nextLine();
+        System.out.println("Hola " + nombre);
+    }
+    
+}
